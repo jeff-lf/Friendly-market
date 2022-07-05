a72d4d9a78b433290b0d3591f704f4c58f2b12b2
danielymanicoba <danielymanicoba@gmail.com>
2022-07-05 18:24:25 -0300

    feat: register form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import "./App.css";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <h1>Oi!</h1>
+    <>
+      <h1>Oi!</h1>
+      <Register />
+    </>
   );
 }
 
---
+++ src/components/Register/index.jsx
@@ -0,0 +1,142 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useHistory } from "react-router-dom";
+
+const Register = () => {
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .required("Campo obrigatório")
+      .matches(/^[A-Za-z]+$/, "Apenas letras"),
+    email: yup
+      .string()
+      .matches(
+        /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i,
+        "Formato de email inválido"
+      )
+      .required("Campo obrigatório"),
+
+    password: yup
+      .string()
+      .required("Campo obrigatório")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&.*])(?=.{8,})/,
+        "Senha deve conter no mínimo 8 caracteres com pelo menos 1 letra maíuscula, 1 número e 1 caractere especial"
+      ),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Senhas não são iguais"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const history = useHistory();
+
+  const registerUser = (data) => {
+    console.log(data);
+    /* api
+      .post("/users", data)
+      .then(() => {
+        toast.success("Conta criada com sucesso!");
+      })
+      .catch(() => {
+        toast.error("Ops! Algo deu errado");
+      }); */
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(registerUser)} className="registerForm">
+        <h2>Crie sua conta</h2>
+        <p>Rapido e grátis, vamos nessa</p>
+
+        <label>
+          Nome
+          <input
+            type="text"
+            {...register("name")}
+            placeholder="Digite aqui seu nome"
+          />
+        </label>
+        {errors.name && <span className="error"> {errors.name.message}</span>}
+
+        <label>
+          Email
+          <input
+            type="text"
+            {...register("email")}
+            placeholder="Digite aqui seu email"
+          />
+        </label>
+        {errors.email && <span className="error"> {errors.email.message}</span>}
+
+        <label>
+          CPF
+          <input
+            type="number"
+            {...register("cpf")}
+            placeholder="Digite aqui seu cpf"
+          />
+        </label>
+        {errors.cpf && <span className="error"> {errors.cpf.message}</span>}
+
+        <label>
+          Telefone
+          <input
+            type="number"
+            {...register("number")}
+            placeholder="Digite aqui seu telefone"
+          />
+        </label>
+        {errors.number && (
+          <span className="error"> {errors.number.message}</span>
+        )}
+        <label>
+          Cidade
+          <input
+            type="text"
+            {...register("city")}
+            placeholder="Digite aqui a cidade"
+          />
+        </label>
+        {errors.city && <span className="error"> {errors.city.message}</span>}
+
+        <label>
+          Senha
+          <input
+            type="password"
+            {...register("password")}
+            placeholder="Digite aqui sua senha"
+          />
+        </label>
+        {errors.password && (
+          <span className="error"> {errors.password.message}</span>
+        )}
+
+        <label>
+          Confirmar senha
+          <input
+            type="password"
+            {...register("confirmPassword")}
+            placeholder="Digite novamente sua senha"
+          />
+        </label>
+        {errors.confirmPassword && (
+          <span className="error"> {errors.confirmPassword.message}</span>
+        )}
+
+        <button type="submit">CADASTRAR</button>
+        {/* <button type="button" onClick={history.goBack}>
+          VOLTAR
+        </button> */}
+      </form>
+    </>
+  );
+};
+
+export default Register;
