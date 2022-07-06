341a5b48f2b8fbd9ad76da438802b711774f43f9
jeff-lf <jefferson_lf@live.com>
2022-07-06 13:32:43 -0300

    feat: redirecionamento a outras paginas pelos botões

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,47 @@
-import { Container } from "./styles"
+import { Container, ModalContainer, PageContainer } from "./styles"
 import { Avatar } from '@mui/material'
 import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify'
+import { useState } from "react"
+import { useHistory } from "react-router-dom"
 
 
 export const Header = () => {
 
+    const [modal, setModal] = useState('')
+
+    const history = useHistory('')
+
     const token = localStorage.getItem('@token')
 
     return (
+        <PageContainer>
         <Container>
             <h1>Friendly Market</h1>
-            <div>
-                <button>Produtos</button>
+            <div className="buttons">
+                <button onClick={() => history.push('/dashboard')}>Produtos</button>
                 <button>Eventos</button>
-                <button>Sobre</button>
+                <button onClick={() => setModal('on')} >Sobre</button>
             </div>
             <div>
                 {token? (<button><Avatar alt="" src="" /><FormatAlignJustifyIcon/></button>) : 
-                    (<button>Entrar</button>)
+                    (<button onClick={() => history.push('/register')}>Entrar</button>)
                 }
                 
             </div>
         </Container>
+
+        <ModalContainer status={modal}>
+                <h3>Não sei oque escrever no titulo</h3>
+                <div>
+                <p>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Saepe voluptate non ea pariatur, 
+                    nostrum officia ad praesentium nam itaque veniam possimus, 
+                    amet est sequi quidem qui libero ipsum, distinctio ipsa.</p>
+                
+                </div>
+                <button onClick={() => setModal('')}>Fechar</button>
+
+        </ModalContainer>
+        </PageContainer>
     )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,36 @@
-export const Container = styled.div`
-    width: 100vw;
-    height: 10vh;
+import styled from "styled-components"
 
+export const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const Container = styled.nav`
+    width: 100vw;
+    height: 8vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    border-bottom: 2px solid black;
+   
+    button{
+    margin: 20px;
+   }
     
+`
+
+export const ModalContainer = styled.div`
+    display: ${props => props.status === "on" ? "block" : "none"};
+    background: #fff;
+    width: 50vw;
+    height: 80vh;
+    margin-top: 6vh;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 1em grey; 
+    z-index: 10;
+    border-radius: 20px;
+
+
 `
