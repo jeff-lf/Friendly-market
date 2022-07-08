b9c8148f4fa2b117f5fa9816beec79aa514f5db2
Nathalia Pinheiro <nathaliapinheiro3@outlook.com>
2022-07-08 10:10:43 -0300

    fix: conflict at Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import CityFilter from "../../components/CityFilter";
 
 const Home = () => {
   return <CityFilter />;
 };
-=======
-const Home = () => {};
->>>>>>> feat/catalogue
 
 export default Home;
