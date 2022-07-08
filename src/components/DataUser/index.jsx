3095384b806d14906c0c39301d6740dfb5aa7e60
danielymanicoba <danielymanicoba@gmail.com>
2022-07-08 17:11:31 -0300

    feat: creating dataUser component

--- src/components/DataUser/index.jsx
+++ src/components/DataUser/index.jsx
@@ -1,3 +1,14 @@
-const DataUser = () => {};
+const DataUser = () => {
+  return (
+    <section className="dataUser">
+      <h3>Bem vinde, User!</h3>
+      <p>Gerenciar e proteger sua conta</p>
+      <div>
+        <p>Nome</p>
+        <p></p>
+      </div>
+    </section>
+  );
+};
 
 export default DataUser;
