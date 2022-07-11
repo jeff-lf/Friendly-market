c6688b2d07bda06559d722e868068a6d37f1aa84
jeff-lf <jefferson_lf@live.com>
2022-07-11 11:46:02 -0300

    feat: atulização do filter (incompleta)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import { Filters } from "./components/Filters";
-
-function App() {
-  return (
-    <>
-      <h1>Oi!</h1>
-      <Filters/>
-    </>
-=======
 import { ToastContainer } from "react-toastify";
 import Routes from "./routes";
 
@@ -18,7 +7,6 @@ function App() {
       <Routes />
       <ToastContainer />
     </div>
->>>>>>> 1a9ebd317097e560fef65127d6c5a0e6da596e91
   );
 }
 
