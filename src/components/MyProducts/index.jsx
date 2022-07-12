cf13b75557b8c0e7ee67aca6f15012b4eb225365
Nathalia Pinheiro <nathaliapinheiro3@outlook.com>
2022-07-12 18:58:06 -0300

    feat: add tabs with dataUser and MyProducts components

--- src/components/MyProducts/index.jsx
+++ src/components/MyProducts/index.jsx
@@ -1,26 +1,39 @@
 import { ContainerMyProducts, StyledH2 } from "./style";
 import { CatalogueContext } from "../../contexts/catalogue/catalogue";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CardMyProduct } from "./cardMyProduct";
-import Button from '../Button/index'
+import Button from '../Button/index';
 import { ModalCadastrar } from "./Modals/cadastrarProduto";
+import { api } from "../../services/api";
 
 
 const MyProducts = () => {
-  const [show, setShow] = useState(false)
-  const { catalogue } = useContext(CatalogueContext);
-  const id = JSON.parse(localStorage.getItem("@Market:id"))
-  const filteredList = catalogue.filter(elem => elem.userId === 1)
 
+  const [show, setShow] = useState(false);
+  const [userData, setUserData] = useState({});
+  const { catalogue } = useContext(CatalogueContext);
+  const id = JSON.parse(localStorage.getItem("@Market:id"));
+  const filteredList = catalogue.filter(elem => elem.userId === 1);
+
+  useEffect(() => {
+    api
+      .get(`/users/${id}`)
+      .then((res) => {
+        setUserData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   
-    const CadastrarProduct = () => {
-        setShow(true)
-    }
-  
+  const CadastrarProduct = () => {
+      setShow(true);
+  };
+
   return (
     <ContainerMyProducts>
-      <StyledH2>Bem vindo, User!</StyledH2>
-      <span>Seus Produtos</span>
+      <div>
+        <StyledH2>Bem vinde, {userData.fullname}!</StyledH2>
+        <span>Visualizar seus produtos</span>
+      </div>
       <div className="listContainer">
         <ul>
           {filteredList?.map((product, index) => (
@@ -28,7 +41,10 @@ const MyProducts = () => {
           ))}
         </ul>
       </div>
-      <Button blue={"blue"} handlerClick={CadastrarProduct} title={"Cadastrar Produto"}></Button>
+
+      <div className="btnRegisterProduct">
+        <Button width={200} blue={"blue"} handlerClick={CadastrarProduct} title={"Cadastrar Produto"}></Button>
+      </div>
       <ModalCadastrar show={show} setShow={setShow}/>
     </ContainerMyProducts>
   );
