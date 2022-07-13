35701aa392e5e7210e04507fd34ca23c4751d8bd
danielymanicoba <danielymanicoba@gmail.com>
2022-07-13 15:19:41 -0300

    style: header mobile menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,9 @@
-import { Container, PageContainer } from "./styles";
+import { Container } from "./styles";
 import { Avatar } from "@mui/material";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Button from "../Button";
 import { api } from "../../services/api";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const Header = () => {
   const history = useHistory("");
@@ -19,15 +20,24 @@ export const Header = () => {
 
   if (id) {
     dataUser();
-  };
-  
+  }
+
   const userImg = localStorage.getItem("@Market:img");
 
   return (
-    <PageContainer>
-      <Container>
-        <h1 className="titleHome" onClick={() => history.push("/")}>Friendly Market</h1>
-        <div className="buttons">
+    <Container>
+      <h1 className="titleHome" onClick={() => history.push("/")}>
+        Friendly Market
+      </h1>
+      <div className="dropdown">
+        <MenuIcon sx={{ fontSize: large }} />
+        <div className="dropdown-content">
+          <Link to="/dashboard">Produtos</Link>
+          <Link to="/aboutUs">Sobre</Link>
+        </div>
+      </div>
+      <div className="buttons">
+        <div className="buttons-nav">
           <Button
             handlerClick={() => history.push("/dashboard")}
             title="Produtos"
@@ -37,24 +47,22 @@ export const Header = () => {
             title="Sobre"
           ></Button>
         </div>
-        <div>
-          {token ? (
-            <button className="avatar">
-              <Avatar
-                alt="foto do usuario"
-                sx={{ bgcolor: "#83D0C8" }}
-                src={userImg}
-              />
-            </button>
-          ) : (
-            <Button
-              handlerClick={() => history.push("/login")}
-              title="Entrar"
-              blue
-            ></Button>
-          )}
-        </div>
-      </Container>
-    </PageContainer>
+        {token ? (
+          <button className="avatar">
+            <Avatar
+              alt="foto do usuario"
+              sx={{ bgcolor: "#83D0C8" }}
+              src={userImg}
+            />
+          </button>
+        ) : (
+          <Button
+            handlerClick={() => history.push("/login")}
+            title="Entrar"
+            blue
+          ></Button>
+        )}
+      </div>
+    </Container>
   );
 };
