cbcb9158e92e435af7067b722ae9c7426d0e7842
Angélica <angeelikasl@gmail.com>
2022-07-13 06:23:40 -0300

    feat: add  filter

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,11 +1,20 @@
 import { useContext } from "react";
 import { CatalogueContext } from "../../contexts/catalogue/catalogue";
+import { FiltersContext } from "../../contexts/filters/filters";
 import Card from "../CardProduct";
 
 const ProductList = () => {
   const { catalogue } = useContext(CatalogueContext);
 
-  return catalogue.map((item, index) => <Card key={index} item={item} />);
+  const { filtered } = useContext(FiltersContext);
+
+  return (
+    <>
+      {filtered.length > 0
+        ? filtered.map((item, index) => <Card key={index} item={item} />)
+        : catalogue.map((item, index) => <Card key={index} item={item} />)}
+    </>
+  );
 };
 
 export default ProductList;
--- src/contexts/filters/filters.jsx
+++ src/contexts/filters/filters.jsx
@@ -4,31 +4,37 @@ import { CatalogueContext } from "../catalogue/catalogue";
 export const FiltersContext = createContext([]);
 
 export const FiltersProvider = ({ children }) => {
-    const { catalogue } = useContext(CatalogueContext)
+  const { catalogue } = useContext(CatalogueContext);
 
-    const [filtered, setFiltered] = useState([])
+  const [filtered, setFiltered] = useState([]);
 
+  const filterInputProducts = (input) => {
+    let filterList = catalogue.filter((product) =>
+      product.category.toLowerCase().includes(input)
+    );
+    let filterListNome = catalogue.filter((product) =>
+      product.productName.toLowerCase().includes(input)
+    );
 
-    const filterInputProducts = (input) => {
-       let filterList =  catalogue.filter((product) => product.category.toLowerCase().includes(input))
-       let filterListNome =  catalogue.filter((product) => product.productName.toLowerCase().includes(input))
+    setFiltered([...filterList, ...filterListNome]);
+  };
 
-       setFiltered([...filterList, ...filterListNome])
-   }
+  const filterProducts = (category) => {
+    if (category === "Todos") {
+      setFiltered(catalogue);
+    } else {
+      let filterList = catalogue.filter(
+        (product) => product.category === category
+      );
+      setFiltered(filterList);
+    }
+  };
 
-   const filterProducts = (category) => {
-       if(category === "Todos"){
-           setFiltered(catalogue)
-       }else{
-           let filterList =  catalogue.filter((product) => product.category === category)
-           setFiltered(filterList)
-       }
-   }
-   
-   return (
-    <FiltersContext.Provider value={{ filtered, filterInputProducts, filterProducts}}>
+  return (
+    <FiltersContext.Provider
+      value={{ filtered, filterInputProducts, filterProducts }}
+    >
       {children}
     </FiltersContext.Provider>
   );
-}
-
+};
